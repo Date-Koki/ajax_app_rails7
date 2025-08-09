b4158c581598d770a007bc0fe7d04119a1b641b6
Date-Koki <k.date.0320@gmail.com>
2025-08-09 14:49:41 +0900

    ajax

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -10,8 +10,7 @@ const buildHTML = (XHR) => {
       </div>
     </div>`;
   return html;
-}
-
+};
 
 function post (){
   const form = document.getElementById("form");
@@ -23,12 +22,9 @@ function post (){
     XHR.responseType = "json";
     XHR.send(formData);
     XHR.onload = () => {
-      if (XHR.status != 200) {
-        alert(`Error ${XHR.status}: ${XHR.statusText}`);
-        return null;
-      };
       const list = document.getElementById("list");
       const formText = document.getElementById("content");
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
       formText.value = "";
     };
   });
